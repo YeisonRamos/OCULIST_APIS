330eefce90510c7fda00101286f60e71ad48f123
Yeison Ramos Rioja <ramosriojayeison@gmail.com>
2026-08-10 15:38:19 -0400

    chore: configurar Firebase en OCULIST

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:oculist/app/oculist_app.dart';
+import 'package:oculist/firebase_options.dart';
 
-import 'app/oculist_app.dart';
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-void main() {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const OculistApp());
 }
